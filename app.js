4148346db21415088cfc47bffbf3a7a796a37b4b
Djeague Prince Dupont <dupontdjeague@gmail.com>
2025-11-04 09:43:04 +0100

    fix: replace app.get('*') with app.use middleware

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'dist')))
 
   // Toutes les routes non-API renvoient vers index.html
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'))
   })
 } else {
